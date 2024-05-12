4c4e7b9976ba39f00fc10576a061de8311d6037b
Sara-597 <longarcher2@gmail.com>
2024-05-12 19:54:43 -0400

    Create loader.user.js

---
+++ loader.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name Themed BC
+// @namespace https://www.bondageprojects.com/
+// @version 1.1.0
+// @description Bondage Club with themes!
+// @author  dDeepLb
+// @match https://bondageprojects.elementfx.com/*
+// @match https://www.bondageprojects.elementfx.com/*
+// @match https://bondage-europe.com/*
+// @match https://www.bondage-europe.com/*
+
+// @grant none
+// @run-at document-end
+// ==/UserScript==
+
+(function () {
+  "use strict";
+  const script = document.createElement("script");
+  script.type = "module";
+  script.setAttribute("crossorigin", "anonymous");
+  script.src = `https://sara-597.github.io/TestScript/test.js?v=${Date.now()}`;
+  document.head.appendChild(script);
+})();
